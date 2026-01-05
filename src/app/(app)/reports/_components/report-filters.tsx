e91a715f4351814205b4259cd0df0174232231ac
biruk getachew <birukgetachew251@gmail.com>
2026-01-05 09:36:52 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@r

--- src/app/(app)/reports/_components/report-filters.tsx
+++ src/app/(app)/reports/_components/report-filters.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { CalendarIcon } from "@radix-ui/react-icons"
+import { CalendarIcon } from "lucide-react"
 import { addDays, format } from "date-fns"
 import { DateRange } from "react-day-picker"
 
