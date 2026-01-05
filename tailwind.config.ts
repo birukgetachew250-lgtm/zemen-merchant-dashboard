0e3898bf8f65e2a2f812bce7f84aa045cd0c2043
biruk getachew <birukgetachew251@gmail.com>
2026-01-05 12:53:21 +0000

    use a moder font family for all the dashboard

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Poppins', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Lexend', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
