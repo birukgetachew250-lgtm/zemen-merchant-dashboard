1a3e2cf3ada88e9cd9329690ecddfc3d906bc55b
biruk getachew <birukgetachew251@gmail.com>
2026-01-05 15:13:26 +0000

    shboard> npx prisma db push Prisma schema loaded from prisma\schema.pris

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,6 @@
 
 import { PrismaClient } from '@prisma/client'
-import { config } from 'dotenv';
-
-config();
+import 'dotenv/config';
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -14,9 +12,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    // The datasourceUrl is now configured in the prisma.config.ts and read from the environment
-    // for the application runtime, so it doesn't need to be explicitly passed here if the
-    // DATABASE_URL environment variable is set.
+    datasourceUrl: process.env.DATABASE_URL,
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
